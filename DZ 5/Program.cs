36f1c30a8548357346752f0409a83be4d3f15bcd
Vialtsev Oleksandr <146359804+VialtsevOleksandr@users.noreply.github.com>
2023-10-02 10:20:44 +0300

    Update Program.cs

--- DZ 5/Program.cs
+++ DZ 5/Program.cs
@@ -29,33 +29,3 @@ public class Mountain : GeographicObject
         return base.GetInformation() + $"Найвища точка: {HighestPoint} м\n";
     }
 }
-class Program
-{
-    static void Main(string[] args)
-    {
-        // Create a River object
-        var river = new River
-        {
-            XCoordinate = 45.678,
-            YCoordinate = -67.890,
-            Name = "Example River",
-            Description = "A beautiful river",
-            CurrentSpeed = 30.5,
-            TotalLength = 1500.0
-        };
-
-        // Create a Mountain object
-        var mountain = new Mountain
-        {
-            XCoordinate = 12.345,
-            YCoordinate = 34.567,
-            Name = "Example Mountain",
-            Description = "A majestic mountain",
-            HighestPoint = 4500.0
-        };
-
-        // Print information about the river and mountain
-        Console.WriteLine("River Information:\n" + river.GetInformation());
-        Console.WriteLine("Mountain Information:\n" + mountain.GetInformation());      
-    }
-}
